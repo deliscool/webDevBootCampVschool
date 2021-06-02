ad71bdd5e1c5add2934103af7c3503eea6f1332f
Lizadel Yarisantos <yarisantos@gmail.com>
2021-06-01 22:48:28 -0600

    Create vacation spot assignment

---
+++ module_3/practice_react/app.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//JSX
+ReactDOM.render(<div><h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
+
+var myNerP = document.createElement("p")
+myNewP.innderHTML = "This is a paragraph."
+
