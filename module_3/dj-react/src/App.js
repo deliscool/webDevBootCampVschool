805cc4e0736180ff690171c6c82fbe99b9d1ba0e
Lizadel Yarisantos <yarisantos@gmail.com>
2021-06-11 15:47:23 -0600

    Make edits

---
+++ module_3/dj-react/src/App.js
@@ -0,0 +1,131 @@
+import './App.css';
+import React from 'react';
+import square from "./components/square";
+
+class App extends React.Component {
+  state = {
+    colors:["white","white", "white", "white"]
+  }
+  smallDj = () => {
+    this.setState(prevState => {
+      return {
+        colors: prevState.colors.map((color) => {
+          if(color !== "white") {
+            return "white"
+          } else {
+            return "pink"
+          }
+        }
+        )}
+    })
+}
+  partyDj = () => {
+    this.setState(prevState => {
+      return {
+        colors: prevState.colors.map((color, index) => {
+          if (index < 2) {
+            return "black"
+          } else {
+            return color
+          }
+        })
+      }
+    })
+  }
+  proDjLeft = () => {
+    this.setState(prevState => {
+        return {
+            colors: prevState.colors.map((color, index) => {
+                if (index === 2) {
+                    return "blue"
+                } else {
+                    return color
+                }
+            })
+        }
+    })
+}
+proDjRight = () => {
+    this.setState(prevState => {
+        return {
+            colors: prevState.colors.map((color, index) => {
+                if(index === 3){
+                    return "green"
+                } else {
+                    return color
+                }
+            })
+        }
+    })
+}
+bigDj1 = () => {
+    this.setState(prevState => {
+        return {
+            colors: prevState.colors.map((color, index) => {
+                if (index === 0 ) {
+                    return "yellow"
+                } else {
+                    return color
+                }
+            })
+        }
+    })
+}
+bigDj2 = () => {
+    this.setState(prevState => {
+        return {
+            colors: prevState.colors.map((color, index) => {
+                if (index === 1 ) {
+                    return "orange"
+                } else {
+                    return color
+                }
+            })
+        }
+    })
+}
+bigDj3 = () => {
+    this.setState(prevState => {
+        return {
+            colors: prevState.colors.map((color, index) => {
+                if (index === 2 ) {
+                    return "red"
+                } else {
+                    return color
+                }
+            })
+        }
+    })
+}
+bigDj4 = () => {
+    this.setState(prevState => {
+        return {
+            colors: prevState.colors.map((color, index) => {
+                if (index === 3 ) {
+                    return "yellow"
+                } else {
+                    return color
+                }
+            })
+        }
+    })
+}
+render() {
+  const squareComp = this.state.colors.map((color) => <square backgroundColor={color} />)
+  return (
+      <div className="container">
+      {squareComp}
+      <button onClick={this.smallDj}>Small Dj</button>
+      <button onClick={this.partyDj}>Party DJ</button>
+      <button onClick={this.proDjLeft}>Pro DJ L</button>
+      <button onClick={this.proDjRight}>Pro DJ R</button>
+      <button onClick={this.bigDj1}>Big Time 1</button>
+      <button onClick={this.bigDj2}>Big Time 2</button>
+      <button onClick={this.bigDj3}>Big Time 3</button>
+      <button onClick={this.bigDj4}>Big Time 4</button>
+      </div>
+  )
+}
+}
+
+export default App
