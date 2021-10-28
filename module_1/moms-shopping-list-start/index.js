9a18b2334a768d2c5f09a9852637bf6daac2457a
Del <mBookpro2021133>
2021-10-27 20:37:17 -0600

    Added comments to my javscript for teaching purposes

--- module_1/moms-shopping-list-start/index.js
+++ module_1/moms-shopping-list-start/index.js
@@ -7,25 +7,32 @@ form.addEventListener("submit", function(event){
     const newLi = document.createElement("li");
     const newDiv = document.createElement("div");
 
+    //Takes value from label id:'title' and creates item in a listed div
     newDiv.textContent = title.value;
     form.title.value = "";
 
+    //Creates new div and list item
     const shopList = document.getElementById("list");
     newLi.append(newDiv);
     shopList.append(newLi);
 
+    //creates edit button
     const editButton = document.createElement('button');
     editButton.textContent = "Edit";
     newLi.append(editButton);
 
+    // Creates delete button
     const dButton = document.createElement('button');
     dButton.textContent = "X";
     newLi.append(dButton);
     
 
+    //eventlistener for delete button to delete list item
     dButton.addEventListener("click", function () {
         newLi.remove();
     })
+
+    //event listener to edit created list item 
     editButton.addEventListener("click", function () {
         newLi.textContent = ("Nope");
     })
